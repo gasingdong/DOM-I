ea317f2ac4bbb90e8a0e19334899a96b86a6c484
TheIllusiveC4 <gasingdong@gmail.com>
2019-06-17 15:02:28 -0700

    finish digital timer stretch

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,10 +1,19 @@
+// Selector queries
 const secondTens = document.querySelector('#secondTens');
 const secondOnes = document.querySelector('#secondOnes');
 const msHundreds = document.querySelector('#msHundreds');
 const msTens = document.querySelector('#msTens');
+const start = document.querySelector('.start');
+const reset = document.querySelector('.reset');
 
+// Initialize starting values
+let count = 0;
+reset.disabled = true;
+
+// Attached to the start button
 function startTimer() {
-  let count = 0;
+  start.disabled = true;
+  reset.disabled = true;
   const timer = setInterval(() => {
     count = count + 0.01;
     secondTens.textContent = Math.floor(count / 10);
@@ -15,8 +24,21 @@ function startTimer() {
     if (count >= 2) {
       clearInterval(timer);
       document.querySelector('.digits').style.color = 'red';
+      reset.disabled = false;
     }
   }, 10);
 }
 
-startTimer();
+// Attached to the reset button
+function resetTimer() {
+  start.disabled = false;
+  count = 0;
+  secondTens.textContent = '-';
+  secondOnes.textContent = '-';
+  msHundreds.textContent = '-';
+  msTens.textContent = '-';
+  document.querySelector('.digits').style.color = 'black';
+}
+
+start.addEventListener('click', () => startTimer());
+reset.addEventListener('click', () => resetTimer());
